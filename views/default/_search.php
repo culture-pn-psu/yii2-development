550ce0644ffce1f6621ff7c7b2904d2e7ffd20be
Madone <ahamad.jedu@gmail.com>
2016-11-02 10:17:38 +0700

    ลบ panel

--- views/default/_search.php
+++ views/default/_search.php
@@ -12,93 +12,92 @@ use kartik\widgets\Select2;
 /* @var $model culturePnPsu\development\models\DevelopmentPersonSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-
-<div class="panel box box-default">
-    <div class="box-header with-border">
-        <h4 class="box-title">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" class="text-muted">
-                <i class="fa fa-search"></i> ค้นหาอย่างละเอียด
-            </a>
-        </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse <?=Yii::$app->request->get('DevelopmentPersonSearch')?'in':''?>" aria-expanded="true">
-        <div class="box-body">
+<h4 class="box-title">
+    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" class="text-muted">
+        <i class="fa fa-search"></i> ค้นหาอย่างละเอียด
+    </a>
+</h4>
 
 
-            <?php
-            $form = ActiveForm::begin([
-                        'action' => ['index'],
-                        'method' => 'get',
-                        'options' => ['data-pjax' => true]
-            ]);
-            ?>
 
-            <div class="row">
-                <div class="col-sm-3">
-                    <?=
-                    $form->field($model, 'user_id')->widget(Select2::classname(), [
-                        'data' => DevelopmentPerson::getPersonList(),
-                        'options' => ['placeholder' => 'ค้นหาบุคลากร'],
-                        'pluginOptions' => [
-                            'allowClear' => true
-                        ],
-                    ]);
-                    ?>
-                </div>
+<div id="collapseOne" class="collapse <?= Yii::$app->request->get('DevelopmentPersonSearch') ? 'in' : '' ?>" aria-expanded="true">
+    <div class="box-body">
 
-                <div class="col-sm-6">
-                    <?= $form->field($model, 'dev_project_id')->dropDownList(DevelopmentProject::getList(), ['prompt' => 'เลือก']) ?>
-                </div>
 
-                <div class="col-sm-3">
-                    <?= $form->field($model, 'dev_activity_char_id')->dropDownList(DevelopmentActivityChar::getList(), ['prompt' => 'เลือก']) ?>
-                </div>
+        <?php
+        $form = ActiveForm::begin([
+                    'action' => ['index'],
+                    'method' => 'get',
+                    'options' => ['data-pjax' => true]
+        ]);
+        ?>
+
+        <div class="row">
+            <div class="col-sm-3">
+                <?=
+                $form->field($model, 'user_id')->widget(Select2::classname(), [
+                    'data' => DevelopmentPerson::getPersonList(),
+                    'options' => ['placeholder' => 'ค้นหาบุคลากร'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);
+                ?>
             </div>
 
-            <div class="row">
-                <div class="col-sm-3">
-                    <?= $form->field($model, 'explodeBy')->dropDownList(DevelopmentPerson::getExplodeBy(), ['prompt' => 'เลือก']) ?>
-                </div>
-
-                <div class="col-sm-3 explodeBy explodeBy-6">    
-                    <?=
-                    $form->field($model, 'start')->widget(DatePicker::className(), [
-                        'pluginOptions' => [
-                            'autoclose' => true,
-                            'format' => 'yyyy-mm-dd'
-                        ]
-                    ])
-                    ?>
-                </div>
-                <div class="col-sm-3 explodeBy explodeBy-6">    
-                    <?=
-                    $form->field($model, 'end')->widget(DatePicker::className(), [
-                        'pluginOptions' => [
-                            'autoclose' => true,
-                            'format' => 'yyyy-mm-dd'
-                        ]
-                    ])
-                    ?>
-                </div>
-
+            <div class="col-sm-6">
+                <?= $form->field($model, 'dev_project_id')->dropDownList(DevelopmentProject::getList(), ['prompt' => 'เลือก']) ?>
             </div>
 
+            <div class="col-sm-3">
+                <?= $form->field($model, 'dev_activity_char_id')->dropDownList(DevelopmentActivityChar::getList(), ['prompt' => 'เลือก']) ?>
+            </div>
+        </div>
 
-
-
-
-
-
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'ค้นหา'), ['class' => 'btn btn-primary']) ?> 
-                <?= Html::resetButton(Yii::t('app', 'ล้าง'), ['class' => 'btn btn-default']) ?>
+        <div class="row">
+            <div class="col-sm-3">
+                <?= $form->field($model, 'explodeBy')->dropDownList(DevelopmentPerson::getExplodeBy(), ['prompt' => 'เลือก']) ?>
             </div>
 
-            <?php ActiveForm::end(); ?>
+            <div class="col-sm-3 explodeBy explodeBy-6">    
+                <?=
+                $form->field($model, 'start')->widget(DatePicker::className(), [
+                    'pluginOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-mm-dd'
+                    ]
+                ])
+                ?>
+            </div>
+            <div class="col-sm-3 explodeBy explodeBy-6">    
+                <?=
+                $form->field($model, 'end')->widget(DatePicker::className(), [
+                    'pluginOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-mm-dd'
+                    ]
+                ])
+                ?>
+            </div>
 
         </div>
+
+
+
+
+
+
+
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'ค้นหา'), ['class' => 'btn btn-primary']) ?> 
+            <?= Html::resetButton(Yii::t('app', 'ล้าง'), ['class' => 'btn btn-default']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
 </div>
+
 
 <?php
 $this->registerCss(' 
